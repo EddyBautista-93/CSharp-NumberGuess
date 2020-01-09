bbef52c870266255b7545df25c4e8d0380852d8a
Eddy Bautista <bautista.j.eddy@gmail.com>
2020-01-09 11:52:18 -0600

    Initial check-in of module numberGuess

---
+++ numberGuess/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace numberGuess
+{
+    class Program
+    {
+        
+
+
+        static void Main(string[] args)
+        {
+            // set app var
+            string appName = "Number Guesser";
+            string appversion = "1.0.0";
+            string appAuthor = "Eddy Bautista";
+            // Change text color
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            //Practice with placeholders
+            Console.WriteLine("{0}: Version {1} by {2}",appName, appversion,appAuthor );
+
+            // reset color
+            Console.ResetColor();
+
+        }
+    }
+}
